712006b7f6e06edffa4e6ed6380f55d70a891078
Etienne <elamotte.ca@gmail.com>
2015-02-28 16:13:04 -0500

    Fix all external link problem (video or not) normally

--- packages/telescope-embedly/lib/client/post_thumbnail.js
+++ packages/telescope-embedly/lib/client/post_thumbnail.js
@@ -3,7 +3,7 @@ Template[getTemplate('postThumbnail')].helpers({
     return !!this.url ? getOutgoingUrl(this.url) : "/posts/"+this._id;
   },
   playVideoClass: function () {
-    return !!this.media ? 'post-thumbnail-has-video': '';
+    return !!this.media ? 'post-thumbnail-has-video': 'post-thumbnail-no-video';
   },
   videoTemplate: function () {
     return getTemplate('postVideo');
@@ -15,5 +15,9 @@ Template[getTemplate('postThumbnail')].events({
     e.preventDefault();
     $('body').addClass('showing-lightbox');
     $(e.target).parents('.post').find('.post-video-lightbox').fadeIn('fast');
+  },
+  'click .post-thumbnail-no-video': function (e) {
+    e.preventDefault();
+    window.open(getOutgoingUrl(this.url),'_blank')
   }
 })
